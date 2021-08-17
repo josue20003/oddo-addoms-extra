9b34efa30f7ebad88189bbe089d8877357da18af
Nestor Josue Márquez Reyes <njosuemreyes@gmail.com>
2021-08-16 22:14:08 -0400

    initial commit, module installable

---
+++ cooking_pot_management/__manifest__.py
@@ -0,0 +1,6 @@
+{
+    "name": "Cooking Put Management", 
+    "version": "14.0.1.0.0",
+    "description": "this module allows the user to keep track of the pots",
+    "author": "Nestor Josue",
+}
